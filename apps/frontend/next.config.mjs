9ddb7266aa36396729eadddfa2071f6b69d7d41a
yoni <jonny@edennetwork.io>
2025-03-27 12:42:42 +0100

    update for frontend build

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     productionBrowserSourceMaps: true,
+    staticPageGenerationTimeout: 120,
     env: {
-        BACKEND_URL: process.env.BACKEND_URL,
+        BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:3000',
       },
 };
 
